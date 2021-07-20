1d778244d22476ee8c36b6ab2078d0b0f4bdfe77
F3licia <romeofelicia@gmail.com>
2021-07-20 12:41:42 +0200

    implementa form registrazione

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,8 +53,9 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            
             'address' => ['required', 'string'],
-            'VAT' => ['required', 'string']
+            'VAT' => [ 'string']
         ]);
     }
 
@@ -70,6 +71,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            
             'address' => $data["address"],
             'VAT' => $data["VAT"]
         ]);
