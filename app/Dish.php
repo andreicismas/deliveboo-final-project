37a74c511215f0f7906a27e9d828d2a5e9f4fcb7
Giulio_Pa <giuliop93@hotmail.it>
2021-07-21 10:08:10 +0200

    aggiunta relazione piatti-utenti

--- app/Dish.php
+++ app/Dish.php
@@ -9,4 +9,9 @@ class Dish extends Model
     public function orders () {
         return $this->belongsToMany("App\Order");
     }
+
+    public function user () {
+        return $this->belongsTo("App\User");
+    }
 }
+
