3b7a517fda5d1272a69de36010a9a81c99fe4ead
F3licia <romeofelicia@gmail.com>
2021-07-23 18:20:52 +0200

    sistemata route show

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ Route::middleware('auth')
 
         //DISHES
         Route::resource("/dishes", "DishController");
+        Route::get("/dishes/{id}", "DishController@show")->name("dishes.show");
     });
 
 //ORDERS UR
