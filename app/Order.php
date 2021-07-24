21d1b0d43c664e7c7cd0788b5fdecd0f4ce4053f
Lex95 <andrea.lega1995@gmail.com>
2021-07-24 17:16:20 +0200

    aggiunta fillable

--- app/Order.php
+++ app/Order.php
@@ -8,7 +8,7 @@ class Order extends Model
 {
 
     protected $fillable = [
-        "delivery_address", "customer_mail"
+        "customer_name", "customer_phone_number", "delivery_address", "customer_mail"
     ];
 
     public function dishes() {
