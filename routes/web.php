4f4fc45ed71e9dfb845dda0c98c50ba3f8ecf3cb
andreicismas <andrei_cismas@yahoo.it>
2021-07-24 19:53:17 +0200

    sdd vue in project laravel

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Type;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -15,7 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    return view('welcome' , [
+        "types"=>Type::all()
+    ]);
 });
 
 Auth::routes();
