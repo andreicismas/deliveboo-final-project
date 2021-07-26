022d671e4e847fc1931fe7a63d399482803201cc
andreicismas <andrei_cismas@yahoo.it>
2021-07-27 01:51:51 +0200

    coretta api user che ritorna user con la relazione type

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -9,12 +9,27 @@ use App\User;
 
 class UserController extends Controller
 {
+// ho comentato l'index xche non ho capito tanto il metodo 'with' in postman mi dava errore
+
+// dopo vari tentativi era  la parola type sbagliata cioe non faceva colegamento /relazione
+
     public function index()
     {
-        $users = User::with("type")->get();
+        $users = User::with("types")->get();
 
         return response()->json([
             'results' => $users
         ]);
     }
+
+    // questa funzione mi restituisce solo  gli users corettamente
+
+    // public function index()
+    // {
+    //     $users = User::all();
+
+    //     return response()->json([
+    //         'results' => $users
+    //     ]);
+    // }
 }
