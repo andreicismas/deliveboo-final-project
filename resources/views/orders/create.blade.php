d5012270ae3f35fdde2660356c944e121192dd24
F3licia <77246060+F3licia@users.noreply.github.com>
2021-08-01 00:03:20 +0200

    .

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -17,7 +17,7 @@
                 <h2>{{$restaurant}}</h2>
             </div>
 
-            <input  class="submit" type="submit" value="ordina">
+            <input  class="my-submit my-bttns" type="submit" value="ordina" value="#ff00ff">
         </div>
          
             @foreach ($dishes as $dish)
@@ -25,35 +25,35 @@
                 
 
                 <div class="display-dish">
-                    <div class="my-overlay"></div>
-
-                    {{--<input type="checkbox" class=""> --}}
+                    <div class="my-overlay"> {{$dish->name}} </div>
 
                     <div  class="description">
                         <h6><strong>Ingredienti:</strong></h6>
                         <h6>{{$dish->ingredients}}</h6>
                        <h6>{{$dish->description}}</h6>
+                       <h3>{{$dish->price}} €</h3>
                     </div>
-                    <div class="my-counter"> 
 
-                         <div class="tag-counter">
-                                <span onclick="this.parentNode.querySelector('input[type=number]').stepDown()" class="minus">-</span>
+                    <div class="counter-div"> 
+
+                        <div class="tag-counter my-bttns">
+                            
+                            <span onclick="this.parentNode.querySelector('input[type=number]').stepUp()" class="plus">+</span>
                             
                                 <input class="counter-display my-counter-input "
                                 name="dishes[{{$dish->id}}]" {{--colleziona gli esatti id che vanno sincronizzati anzichè tutti--}}
                                 type="number"
                                 placeholder="0">
+                            
+                            <span onclick="this.parentNode.querySelector('input[type=number]').stepDown()" class="minus">-</span>
 
-                                <span onclick="this.parentNode.querySelector('input[type=number]').stepUp()" class="plus">+</span>
                         </div>
                     </div>
                 </div>
                 @endif
             @endforeach
-
     
             <input type="hidden" name="restaurant_id" value="{{$dishes[0]->user_id}}">
-
           
         </form>
 
