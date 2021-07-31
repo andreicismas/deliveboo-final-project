012ed899a97d80f6d9ea2d45e7bb6c11cd6e668e
F3licia <77246060+F3licia@users.noreply.github.com>
2021-07-31 10:05:13 +0200

    .

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -1,46 +1,48 @@
 @extends('layouts.app')
 @section('content')
 
-    @if (count($errors->all()) > 0)
-        @foreach ($errors->all() as $error)
-            <h5>{{ $error }}</h5>
-        @endforeach
-    @endif
-
-    <a href="{{ route("welcome") }}">Indietro</a>
-
-    <form action="{{ route('payment') }}" method="post">
-        @csrf
 
 
-        <div v-for="dish in dishes" :key="dish.id">
-            prova
+        @if (count($errors->all()) > 0)
+            @foreach ($errors->all() as $error)
+                <h5>{{ $error }}</h5>
+            @endforeach
+        @endif
+
+
+        <form action="{{ route('payment') }}" method="post">
+            @csrf
+
+        <div class="cart-head">
+            <a href="{{ route("welcome") }}">Indietro</a>
+            <input  class="submit" type="submit" value="ordina">
         </div>
 
-        @foreach ($dishes as $dish)
-
-            @if($dish->visibility == 1)
-            
-            <display-div> </display-div>
-                <input type="checkbox" class=""> 
-             <span>   {{$dish->name}} </span>  
-                <input
-                name="dishes[{{$dish->id}}]" {{--colleziona gli esatti id che vanno sincronizzati anzichè tutti--}}
-                type="number"
-                placeholder="quantity">
-           
-            @endif
-
-
-        @endforeach
-
-        <input type="hidden" name="restaurant_id" value="{{$dishes[0]->user_id}}">
-
-        <div>
-            <input type="submit" value="ordina">
-        </div>
-    </form>
 
    
+         
+            @foreach ($dishes as $dish)
+                @if($dish->visibility == 1)
+                
+
+                <div class="display-dish">
+                    {{--<input type="checkbox" class=""> 
+                    <span>{{$dish->name}} </span>  --}}
+                    <input class="button-form"
+                    name="dishes[{{$dish->id}}]" {{--colleziona gli esatti id che vanno sincronizzati anzichè tutti--}}
+                    type="number"
+                    placeholder="quantity">
+
+
+                 </div>
+                @endif
+            @endforeach
+
+    
+            <input type="hidden" name="restaurant_id" value="{{$dishes[0]->user_id}}">
+
+          
+        </form>
+
 
 @endsection
